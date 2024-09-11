72d4a6e72b7223a82dec7cffe30e84ca05008ee7
minakichi552 <cminaki@dl-techx-xedge.net>
2024-09-11 13:10:02 +0900

    ログアウトの追加

--- EmployeeMenu.js
+++ EmployeeMenu.js
@@ -2,6 +2,11 @@ new Vue({
   el: '#app',
   vuetify: new Vuetify(),
   methods: {
+    logout() {
+      sessionStorage.removeItem('employee_ID');
+      window.location.href = 'EmployeeLogin.html';
+      //window.location.href = 'https://codepen.io/minakichi552/pen/XWLZGrJ?editors=1010'; // ログイン画面のURLに変更
+    },
     stockCheck() {
       
       window.location.href = 'EmployeeStock.html';
