1f54a1b8935504a39312a8ac89e62aacbbf348f6
minakichi552 <cminaki@dl-techx-xedge.net>
2024-09-11 11:24:09 +0900

    遷移先の追加

--- EmployeeMenu.js
+++ EmployeeMenu.js
@@ -1,4 +1,23 @@
 new Vue({
   el: '#app',
   vuetify: new Vuetify(),
+  methods: {
+    stockCheck() {
+      
+      window.location.href = 'EmployeeStock.html';
+      //window.location.href = 'https://codepen.io/minakichi552/pen/XWLZGrJ?editors=1010'; // ログイン画面のURLに変更
+    },
+
+    itemRegister() {
+      
+      window.location.href = 'EmployeeRegister.html';
+      //window.location.href = 'https://codepen.io/minakichi552/pen/XWLZGrJ?editors=1010'; // ログイン画面のURLに変更
+    },
+
+    orderCheck() {
+      
+      window.location.href = 'EmployeeOrder.html';
+      //window.location.href = 'https://codepen.io/minakichi552/pen/XWLZGrJ?editors=1010'; // ログイン画面のURLに変更
+    }
+  }
 });
